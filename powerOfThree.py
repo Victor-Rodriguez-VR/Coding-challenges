fbb061d29846029f7634cba247fa1d007aec0b54
Vic <victor.rodriguez02@student.csulb.edu>
2019-06-07 21:37:22 -0700

    Made the runtime quit a bit faster.
    Its still O(N) but even faster.
    I hope to get it even faster.

--- powerOfThree.py
+++ powerOfThree.py
@@ -1,10 +1,11 @@
 def isPowerOfThree(self, n):
-        currentNumber = 3
-        if(n ==1):
+        ## confirms n is above 3 to be divisible, and 
+        ## modded by 3 to mean it is perfectly divisible
+        while(n>2 and n%3 !=0):
+            n/=3
+        ## 1 is the only answer. Even in its simplest form
+        ## 3/3 =1
+        if(n==1):
             return True
-        while(currentNumber <= n):
-            if(currentNumber== n):
-                return True
-            currentNumber*=3
+        ## anything else is false.
         return False
-        
