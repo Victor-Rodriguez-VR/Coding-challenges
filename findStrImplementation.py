c8069c9a66410a9c1cb636b6a94e4aee3b1c148d
Victor Rodriguez <47678223+Victor-Rodriguez-VR@users.noreply.github.com>
2019-06-21 23:13:00 -0700

    Implement strStr() (python)
    My solution to the following problem:
    Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
    
    Example 1:
    Input: haystack = "hello", needle = "ll"

---
+++ findStrImplementation.py
@@ -0,0 +1,12 @@
+def strStr(haystack, needle):
+  if(len(needle) == 0 or haystack == needle):
+    return 0
+
+  if(len(needle) > len(haystack)):
+    return -1
+
+  for i in range(len(needle),len(haystack)+1):
+    if(haystack[i-len(needle):i] == needle):
+      return (i-len(needle))
+      
+  return -1
