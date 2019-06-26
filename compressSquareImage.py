ed2863d28fb4795b1fdfc04205581f66b9842c76
Victor Rodriguez <47678223+Victor-Rodriguez-VR@users.noreply.github.com>
2019-06-25 20:25:01 -0700

    Perfect square image compression!
    # You are given a perfectly square image as a string, and the size of the image (each side is the same length)
    # Each letter in the string is R, G or B
    # Return the size of the image, followed by an x, followed by the image compressed by "run-length encoding"
    # Run-length encoding has the color, followed by the number of occurences of that color
    # "RRRGGGBBB" with an image of size 3, should return "3xR3G3B3"

---
+++ compressSquareImage.py
@@ -0,0 +1,23 @@
+def compressSquareImage(image, size):
+  # declare final output string number
+  size = str(size)
+  compressedSquareImage = size + "x" 
+  currentLetter = image[0]
+  count = 0
+  # go through the entire string
+  for i in range(len(image)):
+    # check which letter we're looking at
+    if image[i] != currentLetter:
+        compressedSquareImage += currentLetter + str(count)
+        # if not, switch the count that we're adding to
+        currentLetter = image[i]
+        #clear the count of the one we were first adding to 
+        count = 0
+    # add to the count of that letter   
+    count += 1
+  compressedSquareImage += currentLetter + str(count)
+
+  return compressedSquareImage
+    
+
+
