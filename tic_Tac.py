b2ff522d11888dbcefc857c2550864f656f607c1
Vic <victor.rodriguez02@student.csulb.edu>
2019-06-29 23:32:38 -0700

    Complete Tic, but more updates!
    Much like the Sudoku problem, the main programming is complete but I feel as if there is more work to be done. In terms of optimization or clarity in variable names. 
    More Updates soon!

--- tic_Tac.py
+++ tic_Tac.py
@@ -24,21 +24,20 @@ def tic_tac_toe_ROW(board):
       return row[0]
   return 0
     
-
-
 def tic_tac_toe_COLUMN(board):
     for col in range(3):
       if(board[0][col] == board[1][col] == board[2][col] and board[0][col]!= 0 ):
         return (board[0][col])
     return 0
       
-      
-      
+def tic_tac_toe_diagonal(board):
+  if(board[0][0] == board[1][1] == board[2][2] and board[0][0] != 0):
+    return board[0][0]
+  elif(board[0][2] == board[1][1] == board[2][0] and board[0][2]!= 0):
+    return board[0][2]      
+  return 0
+  
 
-
-print("ok")
 print(tic_tac_toe_ROW(tic_tac_toe_row)) # 1
-print("cool")
 print(tic_tac_toe_COLUMN(tic_tac_toe_column))
-#tic_tac_toe(tic_tac_toe_column) # 1
-#tic_tac_toe(tic_tac_toe_diag) #
+print(tic_tac_toe_diagonal(tic_tac_toe_diag)) #
