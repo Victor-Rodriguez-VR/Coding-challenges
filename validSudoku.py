9e08a70882be1f14a45be4c25f9589836c3cc9be
Vic <victor.rodriguez02@student.csulb.edu>
2019-06-29 22:33:09 -0700

    Complete Sudoku, but not done!
    While the program does indeed confirm whether or not a Sudoku board is valid, I feel there are modifications needed to make it reach its maximum potential :)

--- validSudoku.py
+++ validSudoku.py
@@ -1,17 +1,19 @@
 def validate_sudoku_board(board):
-  print("Hello!")
   for row in range(9):
-    if(validateRow(board[row]) == False):
+    if(validateRow(board[row],9) == False):
       return False
 
   for column in range(9):
-    if((validateColumn(board, column) == False)):
+    if((validateColumn(board, column,9) == False)):
       return False
 
-  ##Validating the 3 blocks will go here :)
+  blocks = [0,3,6]
+  for block in blocks:
+    if(validate3s(board,block) == False):
+      return False
   return True
   
-def validateRow(row):
+def validateRow(row, howFar):
   answers = [False] * 9
   for slot in row:
     if(slot != "."):
@@ -22,11 +24,9 @@ def validateRow(row):
         return False
   return True
 
-
-def validateColumn(board, column):
+def validateColumn(board, column, howFar):
   answers = [False] * 9
-  
-  for slot in range(9):
+  for slot in range(howFar):
     if(board[slot][column] != "."):
       numberInSlot = int(board[slot][column])
       if(answers[numberInSlot-1] != True):
@@ -35,5 +35,34 @@ def validateColumn(board, column):
         return False
         
   return True
-  
- 
+
+def validate3s(board,howFar):
+  for row in range(9):
+    if(row % 3 ==0):
+        answers = [False] * 9
+    for slot in range(howFar,howFar+3):
+      if(board[row][slot]!=  "."):
+        numberInSlot = int(board[row][slot])-1
+        if(answers[numberInSlot-1] != True):
+            answers[numberInSlot-1] = True
+        else:
+            return False
+  return True
+
+userInput = input("Select 1 for your own sudoku board.\n Select 2 for an example board")
+board1 = [
+["5","3",".", ".","7",".", ".",".","."],
+["6","4","7", "5","9","8", ".","2","1"],
+[".","9","8", ".",".",".", ".","6","."],
+
+["8",".",".", ".","6",".", ".","5","3"],
+["4",".",".", "8",".","3", ".",".","7"],
+["7",".",".", ".","2",".", ".",".","6"],
+
+[".","6",".", ".",".",".", "2","8","."],
+[".",".",".", "4","1","9", ".",".","5"],
+[".",".",".", ".","8",".", ".","7","9"]
+]
+if(input !=1):
+  board1= userInput
+print(validate_sudoku_board(board1))
