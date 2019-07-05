2713aa39a52a50606e3cc7c15c23d75e275829c2
Vic <victor.rodriguez02@student.csulb.edu>
2019-07-04 17:29:22 -0700

    Made findSmallestNumber faster!
    The question had hints of a binary search being possible, but due to time constraints I could not.
    But, today I snook in some time to get it done.
    Enjoy :)

--- findSmallestNumber.py
+++ findSmallestNumber.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def findSmallestNumber(inputList):
         left = 0
         right = len(inputList)-1
@@ -16,11 +15,3 @@ def findSmallestNumber(inputList):
                         left = pivot
         return smallest
 
-=======
-def findSmallestNumber(inputList):
-    currentSmallest = 0
-    for index in range(1,inputList):
-        if(inputList[index] < inputList[index-1]):
-            return index
-    return 0
->>>>>>> master
