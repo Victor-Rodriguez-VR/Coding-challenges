e58639e87b07e1823fcb624c2868ca80b8f19d91
Vic <victor.rodriguez02@student.csulb.edu>
2019-07-08 20:05:39 -0700

    Merged two solutions
    Completed the merge sort. Sadly its not recursive like it would be in Java, but what can you do ;(

--- mergeSort.py
+++ mergeSort.py
@@ -1,13 +1,15 @@
 def merge_sorted_lists(nums1, nums2):
+
+  nums1 = [1,2,3,0,0,0]
+  nums2 = [2,5,6]
+  # modify num1 in-place
   nums1_Index = 0
   nums2_Index = 0
-  
-  while nums2_Index < len(nums2):
-    if (nums2[nums2_Index] >= nums1[nums1_Index]):
-      #insert nums2[nums2_Index] into nums1[nums1_Index]
-      # remove an element at the end of the list (aka del nums1[len(nums1)-nums1Index]
+  while nums2_Index < len(nums2) :
+    if (nums2[nums2_Index] <= nums1[nums1_Index]) or nums1_Index ==0:
+      nums1.insert(nums1_Index, nums2[nums2_Index])
+      nums1.pop()
       nums2_Index+=1 
     else:
       nums1_Index +=1 
-      
-      
+  
