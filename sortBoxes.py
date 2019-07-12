a4bdf03b545f762aadd2218526aa5da242dc585b
Vic <victor.rodriguez02@student.csulb.edu>
2019-07-11 17:09:04 -0700

    Solved Sort Boxes!
    Solved the LeetCode question of sort boxes. This one was a HUUUGEEE pain but lots of fun. Let me reflect that python for loops are great when you only want to pass through an index once :)

--- sortBoxes.py
+++ sortBoxes.py
@@ -1,18 +1,16 @@
 class Solution(object):
     def sortColors(self, nums):
-         
-        tempBoy = None
-        for n in range(len(nums)-1,0,-1 ):
-            if(nums[n] == 2):
-                del nums[n]
-                nums.append(2)
-            if(nums[n] == 0 and nums[n-1] == 1):
-                tempBoy = nums[n-1]
-                nums[n-1] = nums[n]
-                nums[n] = tempBoy
+        currentIndex = 0
+        lastIndex = len(nums)
         
-            
-        if(nums[0] == 2):
-            del nums[0]
-            nums.append(2)
-    
+        while currentIndex < lastIndex:
+            if(nums[currentIndex] == 2):
+                del nums[currentIndex]
+                nums.append(currentIndex)
+                lastIndex-=1
+                currentIndex-=1
+
+            if(nums[currentIndex] == 0):
+                del nums[currentIndex]
+                nums.insert(0,0)
+            currentIndex+=1
