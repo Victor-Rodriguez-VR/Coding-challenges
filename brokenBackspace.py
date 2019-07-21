b5279d097973fe2afb265d5cd8f12f7b350f498e
Vic <victor.rodriguez02@student.csulb.edu>
2019-07-20 21:43:39 -0700

    No backspace no problem! (Now with stacks)
    Same as before, removes the previous item in a string via a stack, and uses '<' to identify unwanted values.

--- brokenBackspace.py
+++ brokenBackspace.py
@@ -1,9 +1,9 @@
 def k_backspace(inputString):
-  stacko = ''
+  stacko = []
   for character in inputString:
     if(character != "<"):
-      stacko+=character
+      stacko.append(character)
     elif(character == '<' and len(stacko) != 0):
-      stacko = stacko[:len(stacko)-1] + stacko[len(stacko)+1:]
-  return stacko
+      stacko.pop()
+  return "".join(stacko)
 	
