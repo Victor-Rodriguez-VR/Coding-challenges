0ba8b6604b3d4a94246c08e4d0250825ee86125c
Vic <victor.rodriguez02@student.csulb.edu>
2019-07-23 21:51:04 -0700

    Log me maybe? (Laugh's 404'd.)
    Given an array of strings: compile all the data into nested dictionaries that carry a value based on how many times the exact error appears >:)

---
+++ LogAnalysis.py
@@ -0,0 +1,46 @@
+test_data = [
+'[WARNING] 403 Forbidden: No token in request parameters',
+'[ERROR] 500 Server Error: int is not subscriptable',
+'[INFO] 200 OK: Login Successful',
+'[INFO] 200 OK: User sent a message',
+'[ERROR] 500 Server Error: int is not subscriptable'
+]
+
+
+def log_stats(logs):
+  stats = dict()
+  #loop over each line in the log
+  for log in logs:
+    logParts = log.split(" ")
+    description = log.split(":")[1][1:]
+    
+    # find catagory
+    catagory = logParts[0][1:-1]
+    # find http code
+    code = logParts[1]
+    # find the code description
+        #find beginning 
+    start = log.find(code) + len(code) +1
+        #find end
+    end = log.find(":")
+        # apply beginning and end for description
+    description = log[start:end]
+    # find the actual error msg the [1:] ignores some whitespace
+    error = log.split(":")[1][1:]
+    if(stats.get(catagory)):
+      if(stats[catagory].get(code)):
+        if(stats[catagory][code].get(description)):
+          if(stats[catagory][code][description].get(error)):
+            stats[catagory][code][description][error] +=1
+          else:
+            stats[catagory][code][description][error] = 1
+        else:
+          stats[catagory][code][description] = {error:1}
+      else:
+        stats[catagory][code] = {description : {error:1}}
+    else:
+      stats[catagory] = {code:{description:{error:1}}}
+  return stats
+  
+    
+print(log_stats(test_data))
