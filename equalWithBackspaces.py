4a0eedac9e14300766cf3c4d523de95ebc5dbdf3
Vic <victor.rodriguez02@student.csulb.edu>
2019-07-26 21:36:18 -0700

    Slightly faster equalWithBackspaces
    Slightly faster in the fact it is not as reliant on indexing and can make quick comparisons. Posting the same question for consistency. (Also repeated the same code, probably could made a separate function but for now it is indeed faster than the original, but is effectively double its size.)
    
    Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
    
    Example 1:
    Input: S = "ab#c", T = "ad#c"
    Output: true
    Explanation: Both S and T become "ac".

--- equalWithBackspaces.py
+++ equalWithBackspaces.py
@@ -1,20 +1,32 @@
 def backspaceCompare(String1, String2):
-    words = [String1, String2]
-    # Creates space to store backspaced words.
-    backspaced = ["",""]
-    stacky = []
-    for word in range(len(words)):
-        currentWord = words[word]
-        for character in currentWord:
-            if(character != "#"):
-                stacky.append(character)
-            else:
-                if(len(stacky)>0):
-                    stacky.pop()
-        backspaced[word] = backspaced[word].join(stacky)
-        stacky = []
-        
-    return backspaced[0] == backspaced[1]
+    # s1 and s2 are dummies that hold the backspaced strings.
+    s1 = ""
+    s2 = ""
+    # Each dummy uses the same stack.
+    characterStack = []
+    for character in String1:
+        # If the character is not #, adds the character to the stack.
+        if(character != "#"):
+            characterStack.append(character)
+        else:
+            # if the character is #, and the stack has is not empty, removes the lastest item inserted.
+            if(len(characterStack)>0):
+                characterStack.pop()
+            # Otherwise does nothing.
+    # Collapes the items from the stack into a string.
+    s1 = s1.join(characterStack)
+    # resets the stack
+    characterStack = []
+    # repeats lines 7-14 but with the second string.
+    for character in String2:
+        if(character != "#"):
+            characterStack.append(character)
+        else:
+            if(len(characterStack)>0):
+                characterStack.pop()
+    s2= s2.join(characterStack)
+    
+    return s1 == s2
 sampleInput1A = "ab#"
 sampleInput1B = "ab#"
 print(backspaceCompare(sampleInput1A, sampleInput1B))
