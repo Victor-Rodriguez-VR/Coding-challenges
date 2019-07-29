3ed0f9e38d1dda243e19bedd16712c70d118c8eb
Vic <victor.rodriguez02@student.csulb.edu>
2019-07-28 22:08:05 -0700

    Updated documentation!
    Everything is same for now, but wanted to update documentation to make the code flow better.
    
    Given an array of integers, find if the array contains any duplicates.
    
    Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.

--- containsDuplicates.py
+++ containsDuplicates.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def containsDuplicate(self, nums):
+        # Initiates a no-duplicate environment
         trueNumbers = set()
         for number in nums:
+            # If found a duplicate within trueNumbers, immidiately returns true.
             if number in trueNumbers:
                 return True
+            # Otherwise adds the number to trueNumbers.
             else:
                 trueNumbers.add(number)
+        # If no duplicate was found, returns false.
         return False
         
         """
