d3e97d8c0d46fa445e21b175180d8a53fb3c820d
Vic <victor.rodriguez02@student.csulb.edu>
2019-07-31 23:28:23 -0700

    Kowalski, analysis. (nearly correct)
    Implemented the correct string Concatenation. However, a portion of it is still slightly incorrect.

--- Bigram_Analysis.py
+++ Bigram_Analysis.py
@@ -7,11 +7,24 @@ def bigram_frequency_analyzer(text):
         # The past two words.
         twoWords = indivisualWords[words-2] + " " + indivisualWords[words-1]
         currentWord = indivisualWords[words]
-        if (theDict.get(twoWords)):
+        # Determines if the two words are within the dictionary. Incriments the key's value, or creates a value for the key.
+        if(theDict.get(twoWords)):
             if(theDict[twoWords].get(currentWord)):
                 theDict[twoWords][currentWord]+=1
             else:
                 theDict[twoWords][currentWord] = 1
         else:
             theDict[twoWords] = { currentWord : 1}
-    return theDict
+    # Creates value to contain all words and their associated values.
+    wordsAndNumbers = ""
+    for key in theDict:
+        # Concatonates the starting key with a colon.
+        wordsAndNumbers+= key + " : "
+        # Gathers all values from the first key.
+        nestedDictionary = theDict.get(key)
+        for nestedKey in nestedDictionary:
+            # Concatenates the nested keys with their associated value.
+            wordsAndNumbers+= nestedKey + " ("+str(theDict[key].get(nestedKey)) +") "
+        wordsAndNumbers+="\n"
+    return wordsAndNumbers
+
