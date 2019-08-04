0cdbb9906b567ba649ef10c925623dc20696353b
Vic <victor.rodriguez02@student.csulb.edu>
2019-08-03 22:56:09 -0700

    Slight corrections and more consistency.
    Realized in my original upload I had incorrect variable names (like the same variable being named differently) and added some documentation to make the flow of the code more understandable :)

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,18 +1,26 @@
-def isPalindrome(self,s):
-
-    left = 0
-    right = len(s) - 1
-    while right > left:
-        if not s[left].isalnum():
-                i += 1
-        elif not s[right].isalnum():
-            j -= 1
-        elif s[left].lower() == s[right].lower():
-            i += 1
-            j -= 1
-        else:
-            return False
-            
-    return True
+class Solution(object):
+    def isPalindrome(self,theString):
+        # Left starts at beginning of the string, right at the end.
+        left = 0
+        right = len(theString) - 1
+        # while right does not pass left
+        while right > left:
+            # If left is not alphanumeric (numbers or letters)
+            if not theString[left].isalnum():
+                # Skips the letter
+                    left += 1
+            # If right is not alphanumeric (numbers or letters)
+            elif not theString[right].isalnum():
+                # Skips the letter.
+                right -= 1
+            # By  now left and right can only be alphanumeric.
+            # If they are equal to one another, continues.
+            elif theString[left].lower() == theString[right].lower():
+                left += 1
+                right -= 1
+            # If all else fails, it is not palindrome.
+            else:
+                return False
+        # When loop finishes, true. 
+        return True
   
-      
