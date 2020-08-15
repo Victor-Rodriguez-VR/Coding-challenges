aa6e924c873495563993902cb73e1996564183df
Victor Rodriguez <47678223+Victor-Rodriguez-VR@users.noreply.github.com>
2020-08-14 22:55:46 -0700

    Minor documentation (training is draining)

--- reverseString.py
+++ reverseString.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def reverseString(self, s):
-        length = len(s)
-        pivot = len(s) //2
-        for i in range( pivot):
+        length = len(s) 
+        pivot = len(s) //2 # The center of the list/string
+        for i in range(pivot): # We iterate through half of the list/string
             temp = s[i]
-            s[i] = s[length-1-i]
-            s[length-1-i] = temp
-
+            s[i] = s[length-1-i] # Swap farthest and closest based on i.
+            s[length-1-i] = temp 
             
             
 
