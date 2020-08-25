375f09833aae4b773cf7dce431752c289348f052
Victor Rodriguez <47678223+Victor-Rodriguez-VR@users.noreply.github.com>
2020-08-24 22:29:58 -0700

    Working but admittedly lame first submission
    Memory beats 81% but speed beats 21%
    
    I can do better

--- validParentheses.py
+++ validParentheses.py
@@ -1,25 +1,30 @@
-def valid(parentheses):
-    left = "[({"
-    right = "])}"
-    parenStack = [parentheses[0:1]]
-
-    for character in parentheses[1:]:
-        #The left
-        if(left.find(character) != -1):
-            parenStack.append(character)
-        #The right
-        else:
-            if(len(parenStack) >0):
-                lastCharPosition = right.find(character)
-
-                if(lastCharPosition != left.find(parenStack[-1])):
-                    return 0
-
-                elif(lastCharPosition == left.find(parenStack[-1])):
-                    parenStack.pop()
-                    
+class Solution(object):
+    def isValid(self, s):
+        
+        left = "[({"
+        right = "])}"
+        if(len(s) == 0):
+            return True
+        parenStack = [s[0:1]] # Creates a stack with
+        for character in s[1:]:
+            #The left
+            if(left.find(character) != -1):
+                parenStack.append(character)
+            #The right
             else:
-                return 0 
-    return 1
-     
+                if(len(parenStack) >0):
+                    lastCharPosition = right.find(character)
 
+                    if(lastCharPosition != left.find(parenStack[-1])):
+                        return False
+
+                    elif(lastCharPosition == left.find(parenStack[-1])):
+                        parenStack.pop()
+
+                else:
+                    return False 
+        
+        if len(parenStack) != 0:
+            return False
+
+        return True
