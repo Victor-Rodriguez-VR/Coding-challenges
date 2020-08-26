3f447b722f408b2f7fa2f125829501cff8fdd007
Victor Rodriguez <47678223+Victor-Rodriguez-VR@users.noreply.github.com>
2020-08-25 22:07:33 -0700

    I remember my goal for this solution!
    The goal of this one was to not use any built-in functions. (May change to use dictionaries or something)
    
    I'll make another solution that removes that constraint and allows me to use built in functions to speed it up considerably!

--- validParentheses.py
+++ validParentheses.py
@@ -1,30 +1,28 @@
 class Solution(object):
     def isValid(self, s):
-        
         left = "[({"
         right = "])}"
         if(len(s) == 0):
             return True
-        parenStack = [s[0:1]] # Creates a stack with
+        parenStack = [s[0:1]] # Creates a of 
         for character in s[1:]:
-            #The left
+            # If the character is in the left, appends
             if(left.find(character) != -1):
                 parenStack.append(character)
-            #The right
             else:
                 if(len(parenStack) >0):
                     lastCharPosition = right.find(character)
-
+                    # If the leftcharacter's index does not match up with the right's, they're not the same symbol. 
                     if(lastCharPosition != left.find(parenStack[-1])):
                         return False
-
+                    # Otherwise you pop the top item
                     elif(lastCharPosition == left.find(parenStack[-1])):
                         parenStack.pop()
-
+                # If its a right character and no items are left: the parentheses are not valid.
                 else:
                     return False 
-        
+        # if the stack not size 0, then an item is left and is imbalanced. 
         if len(parenStack) != 0:
             return False
-
+        
         return True
